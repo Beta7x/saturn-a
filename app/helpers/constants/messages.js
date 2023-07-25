c2a45850ee25763a8dd50eebb211db082036472e
Beta7x <adepriyantowidies@gmail.com>
2023-07-25 09:59:03 +0700

    [WIDIES][INIT] - Created file for message response

---
+++ app/helpers/constants/messages.js
@@ -0,0 +1,9 @@
+exports.SUCCESS = {
+  REGISTER: 'User created successfully.'
+}
+
+exports.ERROR = {
+  REGISTER: 'User failed to create !.',
+  USER_EXIST: 'User already exists !.',
+  INTERNAL_SERVER: 'The server encountered an internal error or misconfiguration and was unable to complete your request'
+}
