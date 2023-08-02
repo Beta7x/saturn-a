5aae0e275206994ab6a5ceabed7f92856e9a7742
Beta7x <adepriyantowidies@gmail.com>
2023-08-02 08:58:34 +0700

    [WIDIES][CU-860rec12w_BE---Create-API-Login][MODIFY] - Added async function as findUser to find and return user with unique email

--- app/repositories/user_repo.js
+++ app/repositories/user_repo.js
@@ -2,7 +2,15 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const bcrypt = require('bcrypt')
 
-exports.createUser = async (user) => {
+exports.createUser = async user => {
   user.password = bcrypt.hashSync(user.password, 10)
   return await prisma.user.create({ data: user })
+}
+
+exports.findUser = async email => {
+  return await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
 }
