9dc167a4172d776128ae642457bc780be40c1236
Beta7x <adepriyantowidies@gmail.com>
2023-08-02 08:59:42 +0700

    [WIDIES][CU-860rec12w_BE---Create-API-Login][MODIFY] - Added a new async function to handle login request from user

--- app/controllers/auth_controller.js
+++ app/controllers/auth_controller.js
@@ -1,9 +1,18 @@
 const registerService = require('../services/register_service')
+const loginService = require('../services/login_service')
 
 exports.register = async (req, res) => {
-    try {
-        return await registerService.register(req, res)
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    return await registerService.register(req, res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+exports.login = async (req, res) => {
+  try {
+    return await loginService.login(req, res)
+  } catch (error) {
+    console.log(error)
+  }
 }
