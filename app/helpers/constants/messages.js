15840d68e290d9603e0b4ca87d2f6cb85f682348
Beta7x <adepriyantowidies@gmail.com>
2023-08-02 07:09:50 +0700

    [WIDIES][CU-860rec12w_BE---Create-API-Login][MODIFY] - Added error message for handling missing require arguments

--- app/helpers/constants/messages.js
+++ app/helpers/constants/messages.js
@@ -5,5 +5,6 @@ exports.SUCCESS = {
 exports.ERROR = {
   REGISTER: 'User failed to create !.',
   USER_EXIST: 'User already exists !.',
-  INTERNAL_SERVER: 'The server encountered an internal error or misconfiguration and was unable to complete your request'
+  INTERNAL_SERVER: 'The server encountered an internal error or misconfiguration and was unable to complete your request',
+  MISSING_ARGS: 'Required arguments are missing !.'
 }
